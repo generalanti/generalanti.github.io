2b5b24cb043fb00cf8c9e143ba1cdd30ca4b1dda
generalanti <generalanti@gmail.com>
2023-02-10 19:54:39 +0300

    add Jquery support refactor html to add accordion TODO: fix CSS!

--- app.js
+++ app.js
@@ -1,113 +1,61 @@
-let tg = window.Telegram.WebApp;
+// let tg = window.Telegram.WebApp;
 
-tg.expand();
+// tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+// tg.MainButton.textColor = "#FFFFFF";
+// tg.MainButton.color = "#2cab37";
 
-let item = "";
+// let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+// let cost = document.getElementByIdlementById("cost");
+// let slides_new = document.getElementByIdlementById("slides_new");
+// let adaptations_new = document.getElementByIdlementById("adaptations_new");
+// let clippings_normal_new = document.getElementByIdlementById("clippings_normal_new");
+// let clippings_hard_new = document.getElementByIdlementById("clippings_hard_new");
+// let slides_exist = document.getElementByIdlementById("slides_exist");
+// let adaptations_exist = document.getElementByIdlementById("adaptations_exist");
+// let clippings_normal_exist = document.getElementByIdlementById("clippings_normal_exist");
+// let clippings_hard_exist = document.getElementByIdlementById("clippings_hard_exist");
+// let adaptations_other = document.getElementByIdlementById("adaptations_other");
+// let discount = document.getElementByIdlementById("discount");
+// let clear_all = document.getElementByIdlementById("clear_all");
+// let order = document.getElementByIdlementById("order");
+// let lift_up = document.getElementByIdlementById("lift_up");
 
-let cost = document.getElementByIdlementById("cost");
-let slides_new = document.getElementByIdlementById("slides_new");
-let adaptations_new = document.getElementByIdlementById("adaptations_new");
-let clippings_normal_new = document.getElementByIdlementById("clippings_normal_new");
-let clippings_hard_new = document.getElementByIdlementById("clippings_hard_new");
-let slides_exist = document.getElementByIdlementById("slides_exist");
-let adaptations_exist = document.getElementByIdlementById("adaptations_exist");
-let clippings_normal_exist = document.getElementByIdlementById("clippings_normal_exist");
-let clippings_hard_exist = document.getElementByIdlementById("clippings_hard_exist");
-let adaptations_other = document.getElementByIdlementById("adaptations_other");
-let discount = document.getElementByIdlementById("discount");
-let clear_all = document.getElementByIdlementById("clear_all");
-let order = document.getElementByIdlementById("order");
-let lift_up = document.getElementByIdlementById("lift_up");
+// accordion animation
 
-
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
-
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
+$(document).ready(function() {
+	$('.accordion_trigger').click(function() {
+		$(this).next('.accordion_content').slideToggle(200);
+	});
 });
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
 
-let usercard = document.getElementById("usercard");
+// btn1.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 1!");
+// 		item = "1";
+// 		tg.MainButton.show();
+// 	}
+// });
 
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
 
 
-usercard.appendChild(p); 
+// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+// 	tg.sendData(item);
+// });
+
+// let usercard = document.getElementById("usercard");
+
+// let p = document.createElement("p");
+
+// p.innerText = `${tg.initDataUnsafe.user.first_name}
+// ${tg.initDataUnsafe.user.last_name}`;
+
+
+// usercard.appendChild(p); 
 
