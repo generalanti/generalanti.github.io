625dc03554073ad984c3189f596361b19e515c4c
Ilya Razuev <ilya.razuev@gmail.com>
2023-04-22 19:48:25 +0300

    rework js function for styling not empty fields: add text inputs style text inputs

--- js/app.js
+++ js/app.js
@@ -98,7 +98,7 @@ $(document).ready(function () {
 // стилизация непустых числовых вводов input_number в темной зоне
 $(document).ready(function(){
 // применительно к классам числовых вводов
-$('.input_number').blur(function(){
+$('.input_number, .input_text').blur(function(){
     // проверяем, есть ли введенные значения
     let input_val = $.trim(this.value)
     if(input_val.length > 0) {
@@ -111,6 +111,7 @@ $('.input_number').blur(function(){
     }
 })
 });
+
 
 
 
