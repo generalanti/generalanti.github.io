6594030109f8eb3e75d4a070955721bfacefad5f
Ilya Razuev <ilya.razuev@gmail.com>
2023-05-11 21:03:40 +0300

    TEST inidData transfer from TG - change to user.id

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ tg.expand();
 // изменяем значение стоимости
 // let cost_value = 1000000;
 var cost = document.querySelector('#cost')
-cost.textContent = tg.initDataUnsafe.username + "₽";
+cost.textContent = tg.initDataUnsafe.user.id + "₽";
 
 
 // функция анимации аккордеона
