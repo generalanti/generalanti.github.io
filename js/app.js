139efa993829216fb790e0d56b3c096ce5d98d6f
Ilya Razuev <ilya.razuev@gmail.com>
2023-05-13 21:00:58 +0300

    scroll upp action is moved to a separate function (scroll_up) call scroll_up func when clear_all

--- js/app.js
+++ js/app.js
@@ -16,157 +16,8 @@ try {
 // "₽";
 
 
-// функция анимации аккордеона
-$(document).ready(function () {
-    // работаем по событию клик по триггеру
-    $('.accordion_trigger').click(function () {
-        // если аккордеон развернут, то сворачиваем
-        if ($(this).parent().hasClass('accordion_item_active')) {
-            $(this).parent().removeClass('accordion_item_active')
-        }
-        // если свернут, то разворачиваем и скроллим всю страницу к началу триггера
-        else {
-            $(this).parent().addClass('accordion_item_active');
-            // скроллим к позиции триггера + вниз на n пикселей, чтобы стоимость
-            // не перекрывала лэйбл триггера
-            setTimeout(() => {
-                $('body, html').animate({
-                    scrollTop: $(this).offset().top - 92
-                }, 500)
-            }, 400)
-        }
-
-    });
-});
 
 
-// функция добавления класса при скролле
-$(document).ready(function () {
-    // const el_offset = $('#scrollable_el').offset().top; // значение отступа сверху элемента
-    $(window).scroll(function () {
-        const scrolled = $(this).scrollTop(); // то, насколько мы проскроллили
-        let scrollable_area = $('#scrollable_area')
-        if (scrolled > 1) {
-            // элемент прилип. добавляем класс на всю площадь скролла,
-            // так как только так можно явно задать поведение для всех
-            // элементов внутри
-            scrollable_area.addClass('el-fixed');
-        } else if (scrolled < 1) {
-            // элемент отлип. убираем класс
-            scrollable_area.removeClass('el-fixed');
-
-        }
-
-    });
-});
-
-
-// Кнопка вверх
-// Функция появления и скрытия кнопки
-$(document).ready(function () {
-    // при скролле больше 200 px показать кнопку, иначе скрыть
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 150) {
-            $('#scrolling').fadeIn();
-        } else {
-            $('#scrolling').fadeOut(400);
-        }
-    });
-    // функция плавного скролла вверх
-    $('#scrolling').click(function () {
-        $('body, html').animate({
-            scrollTop: 0
-        }, 550);
-        return false;
-    });
-});
-
-// input_number
-// стилизация непустых числовых вводов input_number в темной зоне
-$(document).ready(function () {
-// применительно к классам числовых вводов
-    $('.input_number, .input_text').blur(function () {
-        // проверяем, есть ли введенные значения
-        let input_val = $.trim(this.value)
-        if (input_val.length > 0) {
-            // если есть, то добавляем к ним класс
-            $(this).addClass('input_not_empty');
-        } else {
-            // и убираем, если значений нет
-            $(this).removeClass('input_not_empty');
-        }
-    })
-});
-
-// функция добавления "%" при вводе числа скидки (и удалении "%" если цифр нет)
-$(document).ready(function () {
-    let discount = $('#discount')
-    discount.change(function () {
-        let input_val = $.trim(this.value)
-        if (input_val.length > 0) {
-            $(this).val(function (index, old) {
-                return old.replace(/[^0-9]/g, '') + '%';
-            });
-        }
-    });
-    discount.blur(function () {
-        let input_val = $.trim(this.value)
-        if (input_val === '%') {
-            $(this).val(function (index, old) {
-                return old.replace('%', '');
-            });
-        }
-    });
-});
-
-
-// функция стирания данных из инпутов расчета
-$(document).ready(function () {
-    $('#clear_all').click(function () {
-        $('.input_number, #promocode').val('');
-        $('.input_number, .input_text').removeClass('input_not_empty');
-        $('input[type="checkbox"]').prop("checked", false)
-    });
-});
-
-
-// отключение чекбоксов, если данные в кол-во слайдов не введены
-function disable_checkbox(input_obj, checkbox_obj) {
-    if (!input_obj.val() || input_obj.val() < 2) {
-        checkbox_obj.attr("disabled", true)
-    } else {
-        checkbox_obj.removeAttr("disabled")
-    }
-}
-
-$('#slides_new').on("input", function () {
-    disable_checkbox($('#slides_new'), $('#adaptations_new'))
-})
-
-$('#slides_exist').on("input", function () {
-    disable_checkbox($('#slides_exist'), $('#adaptations_exist'))
-})
-
-
-// валидация числовых инпутов на темном фоне - можно вводить только числа
-$('body').on('input', '.input_number.dark_bg_content', function () {
-    this.value = this.value.replace(/[^0-9]/g, '');
-});
-
-// валидация числовых инпутов на светлом фоне - можно вводить только числа
-$('body').on('input', '.input_number.light_bg_content', function () {
-    this.value = this.value.replace(/[^0-9%]/g, '');
-});
-
-// валидация текстовых инпутов имени и фамилии
-$('body').on('input', '#name, #last_name', function () {
-    this.value = this.value.replace(/[^a-zA-Zа-яА-Я]/g, '');
-});
-
-// валидация текстового инпута промокода
-$('body').on('input', '#promocode', function () {
-    this.value = this.value.replace(/[^0-9a-zA-Zа-яА-Я]/g, '');
-});
 
 
 // Калькулятор цен
@@ -277,6 +128,178 @@ function calculate_cost() {
         cost_adaptations_other
     $('#cost').text(total_cost)
 }
+
+// функция анимации аккордеона
+$(document).ready(function () {
+    // работаем по событию клик по триггеру
+    $('.accordion_trigger').click(function () {
+        // если аккордеон развернут, то сворачиваем
+        if ($(this).parent().hasClass('accordion_item_active')) {
+            $(this).parent().removeClass('accordion_item_active')
+        }
+        // если свернут, то разворачиваем и скроллим всю страницу к началу триггера
+        else {
+            $(this).parent().addClass('accordion_item_active');
+            // скроллим к позиции триггера + вниз на n пикселей, чтобы стоимость
+            // не перекрывала лэйбл триггера
+            setTimeout(() => {
+                $('body, html').animate({
+                    scrollTop: $(this).offset().top - 92
+                }, 500)
+            }, 400)
+        }
+
+    });
+});
+
+
+// функция добавления класса при скролле
+$(document).ready(function () {
+    // const el_offset = $('#scrollable_el').offset().top; // значение отступа сверху элемента
+    $(window).scroll(function () {
+        const scrolled = $(this).scrollTop(); // то, насколько мы проскроллили
+        let scrollable_area = $('#scrollable_area')
+        if (scrolled > 1) {
+            // элемент прилип. добавляем класс на всю площадь скролла,
+            // так как только так можно явно задать поведение для всех
+            // элементов внутри
+            scrollable_area.addClass('el-fixed');
+        } else if (scrolled < 1) {
+            // элемент отлип. убираем класс
+            scrollable_area.removeClass('el-fixed');
+
+        }
+
+    });
+});
+
+
+// функция плавного скролла вверх
+function scroll_up() {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 550);
+    return false;
+}
+
+// Кнопка вверх
+// Функция появления и скрытия кнопки
+$(document).ready(function () {
+    // при скролле больше 200 px показать кнопку, иначе скрыть
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('#scrolling').fadeIn();
+        } else {
+            $('#scrolling').fadeOut(400);
+        }
+    });
+    // скролл вверх
+    $('#scrolling').click(function () {
+        scroll_up()
+    });
+});
+
+// input_number
+// стилизация непустых числовых вводов input_number в темной зоне
+$(document).ready(function () {
+// применительно к классам числовых вводов
+    $('.input_number, .input_text').blur(function () {
+        // проверяем, есть ли введенные значения
+        let input_val = $.trim(this.value)
+        if (input_val.length > 0) {
+            // если есть, то добавляем к ним класс
+            $(this).addClass('input_not_empty');
+        } else {
+            // и убираем, если значений нет
+            $(this).removeClass('input_not_empty');
+        }
+    })
+});
+
+// функция добавления "%" при вводе числа скидки (и удалении "%" если цифр нет)
+$(document).ready(function () {
+    let discount = $('#discount')
+    discount.on("input", function () {
+        // let input_val = this.value
+        if (this.value.length > 0) {
+            $(this).val(function (index, old) {
+                return old.replace(/[^0-9]/g, '') + '%';
+            });
+        }
+    });
+    discount.blur(function () {
+        let input_val = $.trim(this.value)
+        if (input_val === '%') {
+            $(this).val(function (index, old) {
+                return old.replace('%', '');
+            });
+        }
+    })
+    discount.bind("paste", function(e){
+        // access the clipboard using the api
+        var pastedData = e.originalEvent.clipboardData.getData('text');
+        if (pastedData.length > 0) {
+            $(this).val(function (index, old) {
+                return old.replace(/[^0-9]/g, '') + '%';
+            });
+        }
+    } );
+    ;
+});
+
+
+// функция стирания данных из инпутов расчета
+$(document).ready(function () {
+    $('#clear_all').click(function () {
+        $('.input_number, #promocode').val('');
+        $('.input_number, .input_text').removeClass('input_not_empty');
+        $('input[type="checkbox"]').prop("checked", false)
+        disable_checkbox($('#slides_new'), $('#adaptations_new'))
+        disable_checkbox($('#slides_exist'), $('#adaptations_exist'))
+        scroll_up()
+    });
+});
+
+
+// отключение чекбоксов, если данные в кол-во слайдов не введены
+function disable_checkbox(input_obj, checkbox_obj) {
+    if (!input_obj.val() || input_obj.val() < 2) {
+        checkbox_obj.attr("disabled", true)
+    } else {
+        checkbox_obj.removeAttr("disabled")
+    }
+}
+
+$('#slides_new').on("input", function () {
+    disable_checkbox($('#slides_new'), $('#adaptations_new'))
+})
+
+$('#slides_exist').on("input", function () {
+    disable_checkbox($('#slides_exist'), $('#adaptations_exist'))
+})
+
+
+// валидация числовых инпутов на темном фоне - можно вводить только числа
+$('body').on('input', '.input_number.dark_bg_content', function () {
+    this.value = this.value.replace(/[^0-9]/g, '');
+});
+
+// валидация числовых инпутов на светлом фоне - можно вводить только числа
+$('body').on('input', '.input_number.light_bg_content', function () {
+    this.value = this.value.replace(/[^0-9%]/g, '');
+});
+
+// валидация текстовых инпутов имени и фамилии
+$('body').on('input', '#name, #last_name', function () {
+    this.value = this.value.replace(/[^a-zA-Zа-яА-Я]/g, '');
+});
+
+// валидация текстового инпута промокода
+$('body').on('input', '#promocode', function () {
+    this.value = this.value.replace(/[^0-9a-zA-Zа-яА-Я]/g, '');
+});
+
+
 
 
 // btn1.addEventListener("click", function(){
