ea7d21fe3bdf14ebcb7233d32f7ae1a3f94902e2
Ilya Razuev <ilya.razuev@gmail.com>
2023-05-14 20:21:34 +0300

    add sql connection code (commented) TODO: fix selecting number (drops selection)

--- js/app.js
+++ js/app.js
@@ -13,6 +13,26 @@ try {
 
 }
 
+// подключение к MySQL
+// const mysql = require('mysql2');
+//
+// // create the connection to database
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     pass: 123,
+//     database: 'test'
+// });
+//
+// // simple query
+// connection.query(
+//     'SELECT * FROM `table',
+//     function(err, results, fields) {
+//         console.log(results); // results contains rows returned by server
+//         console.log(fields); // fields contains extra meta data about results, if available
+//     }
+// );
+
 
 // Калькулятор цен
 
